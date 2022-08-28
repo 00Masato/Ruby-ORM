235898eb05c1cca9745816580d21913f7ff7ae7e
Kato Masato <masato.kato0604@gmail.com>
2022-08-28 09:40:16 +0900

    add read file method

--- lib/RubyORM/runner.rb
+++ lib/RubyORM/runner.rb
@@ -4,7 +4,9 @@ module RubyORM
   class Runner < Thor
     desc "generate", "an example task"
     def generate
-      puts "this method is generate"
+      schema_path = './spec/db/Schemafile'
+      File.open(schema_path)
+      puts File.read(schema_path)
     end
   end
 end
---
+++ spec/ruby_orm/runner_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+class User < RubyORM::Base
+end
+
+RSpec.describe 'Runner' do
+
+end
