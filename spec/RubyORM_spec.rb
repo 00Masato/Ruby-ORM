4918ae4632efc550b1e574aa7fb539cc7c0f6acf
Kato Masato <masato.kato0604@gmail.com>
2022-10-01 16:13:59 +0900

    delete unnecessary test

--- spec/RubyORM_spec.rb
+++ spec/RubyORM_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RubyORM do
   it "has a version number" do
     expect(RubyORM::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
