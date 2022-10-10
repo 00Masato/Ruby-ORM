6f792c8bbca6159e195139ac5c4b930b68e1b174
Kato Masato <masato.kato0604@gmail.com>
2022-10-10 14:28:04 +0900

    add generate command

--- lib/RubyORM/runner.rb
+++ lib/RubyORM/runner.rb
@@ -1,12 +1,21 @@
-require 'thor'
+require "RubyORM"
 
 module RubyORM
-  class Runner < Thor
-    desc "generate", "an example task"
-    def generate
-      schema_path = './spec/db/Schemafile'
+  class Runner
+    def self.generate
+      RubyORM.connection = Mysql2::Client.new(
+        host: 'localhost',
+        username: 'root',
+        password: 'password',
+        database: 'ruby_orm_sample'
+      )
+      schema_path = "./spec/db/Schemafile"
       File.open(schema_path)
-      puts File.read(schema_path)
+      sqls = File.read(schema_path).strip.split(";");
+      schema = RubyORM::Base.new
+      sqls.each do |sql|
+        schema.query(sql)
+      end
     end
   end
 end
